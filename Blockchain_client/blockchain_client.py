7ad64a8bedf5cd9a14d048e18727b760d2e3caed
Snow <164378011+TheSnowcloak@users.noreply.github.com>
2025-03-24 11:09:30 +0100

    language correction

--- Blockchain_client/blockchain_client.py
+++ Blockchain_client/blockchain_client.py
@@ -146,7 +146,7 @@ def upload_form():
         enc_nonce_b64 = nonce_b64
         enc_tag_b64   = tag_b64
 
-        app.logger.info(f"*** CITLIVÝ soubor => stored at {local_abs}")
+        app.logger.info(f"*** SENSITIVE file => stored at {local_abs}")
         app.logger.info(f"   encryption key (base64) = {key_b64}")
         app.logger.info(f"   nonce = {nonce_b64}")
         app.logger.info(f"   tag   = {tag_b64}")
